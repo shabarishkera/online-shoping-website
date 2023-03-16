e7eaf7f90d3ad4ac00854f476f571b46cb4131c3
shabarish <shabarishkera@gmail.com>
2023-03-16 22:52:30 +0530

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='nav-container'>
+    <ul>
+<li><a href='#'>HOME</a></li>
+<li><a href='#'>CLOTH</a></li>
+<li><a href='#'>GADGETS</a></li>
+<li><a href='#'>ELECTRONICS</a></li>
+<li><a href='#'>FOOD</a></li>
+<li><a href='#'>FURNITURE</a></li>
+<li><a href='#'>OTHERS</a></li>
+</ul>
+</div>
+  )
+}
