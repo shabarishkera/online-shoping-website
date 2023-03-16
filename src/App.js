6dfad6e26c0139076d6bbdfff844ad2df384a662
shabarish <shabarishkera@gmail.com>
2023-03-16 23:54:50 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card';
-import { useEffect, useState } from 'react';
-import Navbar from './components/Navbar';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import Cardblock from './components/CardBoard'
 function  App() {
   
-  const [data,setdata]=useState([]);
-  const  fetchdata= async()=>{
-   const responce= await fetch("https://fakestoreapi.com/products?limit=15");
-   const dat=await responce.json();
-   setdata(dat);
-
-  };
-  useEffect(()=>{fetchdata();},[]);
-  console.log(data[0]);
+ 
   return (
-    <div className="App">
-      <div className='navbar-container'>
+    <div className="">
+     
       <Navbar />
-      </div>
-      <div  className='card-container'>
- { data&&data.map((item)=>{
-return (
- <Card img={item.image} rating={item.rating.rate} round={item.rating.count} title={item.title} price={item.price} key={item.id}/>);
- })
-}
-
-    </div>
+  <Cardblock url="https://fakestoreapi.com/products?limit=15"/>
+     
     </div>
   );
 }
